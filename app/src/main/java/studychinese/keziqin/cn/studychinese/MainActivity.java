49faa8acbb5a7c1b567961edc8b16ce43e6764ff
DozenPiggy <dozenpiggy@163.com>
2018-04-03 22:22:26 +0800

    first commit

---
+++ app/src/main/java/studychinese/keziqin/cn/studychinese/MainActivity.java
@@ -0,0 +1,13 @@
+package studychinese.keziqin.cn.studychinese;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
